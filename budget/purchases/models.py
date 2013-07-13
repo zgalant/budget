ce807e3beb8b3301e69c8588ce7182c9ba4748f9
zgalant <zach.galant@gmail.com>
2013-07-13 10:49:50 -0700

    add basic models for Purchase, Tag, migration

--- budget/purchases/models.py
+++ budget/purchases/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+"""Models for the main purchases app."""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User)
+    description = models.CharField(max_length=1000)
+    price = models.FloatField()
+    timestamp = models.DateField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='purchases')
