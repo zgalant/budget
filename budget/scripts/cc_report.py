1abaf751e0c1eb9ea870a0bac7323da4e95c7903
zgalant <zach.galant@gmail.com>
2014-05-30 21:46:34 -0700

    add script to parse, add purchses for a credit card report

---
+++ budget/scripts/cc_report.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from purchases.models import Purchase
+from purchases.models import Tag
+
+import datetime
+from datetime import date
+
+
+def get_date(date_str):
+    date_str = date_str.replace("\"", "")
+    parts = date_str.split("/")
+    month = int(parts[0])
+    day = int(parts[1])
+    year = int(parts[2])
+    d = datetime.date(month=month, day=day, year=year)
+    return d
+
+
+def run(*script_args):
+    user = User.objects.get(username="zg")
+    filename = script_args[0]
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+    lines = lines[3:]
+    for line in lines:
+        purchase = line.split(",")
+        dt = get_date(purchase[0])
+        description = purchase[1]
+        price = float(purchase[2][1:])
+        tags = purchase[3:]
+        p = Purchase(
+            description=description,
+            price=price,
+            user=user
+        )
+        p.timestamp = dt
+        p.save()
+        print price
+        for tag in tags:
+            tag = tag.replace("\r\n", "").replace("\"", "")
+            tag = tag.strip()
+            print tag
+            t, created = Tag.objects.get_or_create(name=tag)
+            t.save()
+            p.tags.add(t)
+        print ""
